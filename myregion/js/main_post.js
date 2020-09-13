a1d574131f24ff1ad34f78964ffff885d30b6302
sevahasla <sevastesl@gmail.com>
2020-09-13 15:35:19 +0300

    Add files via upload

--- myregion/js/main_post.js
+++ myregion/js/main_post.js
@@ -1,10 +1,15 @@
-var x = "Хочу построить ещё одну шаурмечную на углу!";
+var x = "Хочу построить фонтан к дню ВДВ!";
 var cnt = 0; 
 function MyPrint(){
 	document.getElementById("post_place").innerHTML = x;
 }
 
 function MyIncreaseCnt(){
-	document.getElementById("Increase").innerHTML = String(cnt + 1);
 	cnt += 1;
+	if(cnt % 10 >= 2){
+		document.getElementById("Increase").innerHTML = String(cnt) + String(" помогут!");
+	}
+	else{
+		document.getElementById("Increase").innerHTML = String(cnt) + String(" поможет!");
+	}
 }
